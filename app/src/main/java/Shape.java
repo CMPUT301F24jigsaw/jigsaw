c9743c2badbf90aaea7ac14efff6ec06d7800601
Ash0106 <ashwinshanmugam.com>
2024-09-27 15:24:52 -0600

    blue

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -3,7 +3,7 @@ package jigsaw.app.src.main.java;
 public abstract class Shape {
     public int x;
     public int y;
-    String color = "blue";
+    String color = "White";
     
     public Shape(int x, int y) {
         this.x = x;
