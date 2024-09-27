ad80f4f0e355ac071fd592ae50cd804701cfeb06
Ash0106 <ashwinshanmugam.com>
2024-09-27 14:27:49 -0600

    blue colour added

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -3,7 +3,8 @@ package jigsaw.app.src.main.java;
 public abstract class Shape {
     public int x;
     public int y;
-    
+    String color = "blue";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
