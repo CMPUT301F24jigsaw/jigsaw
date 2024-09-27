f66d12b92f0e781d4b999cd1a8b433d79275ab3d
Ash0106 <ashwinshanmugam.com>
2024-09-27 14:03:50 -0600

    Circle added

---
+++ app/src/main/java/Circle.java
@@ -0,0 +1,7 @@
+package jigsaw.app.src.main.java;
+
+public class Circle extends Shape{
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+}
--- app/src/main/java/Star.java
+++
@@ -1,7 +0,0 @@
-package jigsaw.app.src.main.java;
-
-public class Star extends Shape{
-    public Star(int x, int y) {
-        super(x, y);
-    }
-}
