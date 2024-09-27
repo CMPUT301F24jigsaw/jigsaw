a103170e3b039d4766ef326e8f377777553ea976
Rishi Renchen <rishirenchen007@gmail.com>
2024-09-27 13:15:13 -0600

    Rectangle added

---
+++ app/src/main/java/com/example/jigsaw/Rectangle.java
@@ -0,0 +1,9 @@
+package jigsaw.app.src.main.java.com.example.jigsaw;
+
+import jigsaw.app.src.main.java.Shape;
+
+public class Rectangle extends Shape {
+    public Rectangle(int x, int y) {
+        super(x, y);
+    }
+}
